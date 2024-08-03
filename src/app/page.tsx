29e8395695bd2f61b06a1c576bced242fe62ad31
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-03 19:18:36 +0530

    set homepage structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 const Homepage = () => {
-  return <div className="">Homepage</div>;
+  return (
+    <div className="flex gap-6 pt-6">
+      <div className="hidden xl:block w-[20%]">LeftMenu</div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          stories <br /> addpost <br /> feed
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">rightmenu</div>
+    </div>
+  );
 };
 
 export default Homepage;
