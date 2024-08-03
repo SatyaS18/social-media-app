85d92a4bb6890e7b28545db568f1f98c38f37016
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-03 20:09:17 +0530

    add post component

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,5 +1,48 @@
+import Image from "next/image";
+
 const AddPost = () => {
-  return <div>AddPost</div>;
+  return (
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src="/noAvatar.png"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+
+      {/* POST  */}
+      <div className="flex-1">
+        {/* TEXT INPUT  */}
+
+        <form
+          //   action={(formData) => addPost(formData, img?.secure_url || "")}
+          className="flex gap-4"
+        >
+          <textarea
+            placeholder="What's on your mind?"
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+            name="desc"
+            // onChange={(e) => setDesc(e.target.value)}
+          ></textarea>
+          <div className="">
+            <Image
+              src="/emoji.png"
+              alt=""
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+            {/* <AddPostButton /> */}
+          </div>
+        </form>
+
+        {/* POST OPTIONS  */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
