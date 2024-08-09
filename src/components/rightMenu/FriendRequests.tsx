cdd3d883f451eadfdb9071759381d63c15e06d95
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-09 14:25:44 +0530

    add link

--- src/components/rightMenu/FriendRequests.tsx
+++ src/components/rightMenu/FriendRequests.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const FriendRequests = () => {
-  return <div>FriendRequests</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* top */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">Friend Requests</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+
+      {/* user  */}
+    </div>
+  );
 };
 
 export default FriendRequests;
