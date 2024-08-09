5427f96a84394e122d0b54263f330b0fcaefb136
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-09 14:22:49 +0530

    add friend request component

--- src/components/rightMenu/RightMenu.tsx
+++ src/components/rightMenu/RightMenu.tsx
@@ -1,5 +1,11 @@
-const RightMenu = () => {
-  return <div>RightMenu</div>;
+import FriendRequests from "./FriendRequests";
+
+const RightMenu = ({ userId }: { userId?: string }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <FriendRequests />
+    </div>
+  );
 };
 
 export default RightMenu;
