d49cf18f4e1d43e4e6916c01967e937166c19923
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-09 14:34:02 +0530

    add style

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,5 +1,5 @@
 const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
-  return <div className="">Ad</div>;
+  return <div className="p-4 bg-white rounded-lg shadow-md text-sm ">Ad</div>;
 };
 
 export default Ad;
--- src/components/rightMenu/Birthdays.tsx
+++ src/components/rightMenu/Birthdays.tsx
@@ -1,5 +1,9 @@
 const Birthdays = () => {
-  return <div className="">Birthdays</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      Birthdays
+    </div>
+  );
 };
 
 export default Birthdays;
