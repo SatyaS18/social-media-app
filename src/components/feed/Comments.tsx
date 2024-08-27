5cc14e2b546c2679f9bbff502910736b81e95de7
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-27 21:20:10 +0530

    add comments for each post

--- src/components/feed/Comments.tsx
+++ src/components/feed/Comments.tsx
@@ -1,81 +1,20 @@
+import prisma from "@/lib/client";
 import Image from "next/image";
+import CommentList from "./CommentList";
 
-const Comments = () => {
+const Comments = async ({ postId }: { postId: number }) => {
+  const comments = await prisma.comment.findMany({
+    where: {
+      postId,
+    },
+    include: {
+      user: true,
+    },
+  });
   return (
     <div>
-      <div>
-        {/* write  */}
-        <div className="flex items-center gap-4">
-          {" "}
-          <Image
-            // src={user.imageUrl || "noAvatar.png"}
-            src="/noAvatar.png"
-            alt=""
-            width={32}
-            height={32}
-            className="w-8 h-8 rounded-full"
-          />
-          <div className="flex-1 flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
-            <input
-              type="text"
-              placeholder="Write a comment..."
-              className="bg-transparent outline-none flex-1"
-              //   onChange={(e) => setDesc(e.target.value)}
-            />
-            <Image
-              src="/emoji.png"
-              alt=""
-              width={16}
-              height={16}
-              className="cursor-pointer"
-            />
-          </div>
-        </div>
-        {/* comments  */}
-        <div>
-          {/* comment  */}
-          <div className="flex gap-4 justify-between mt-6">
-            {/* avatar  */}
-            <Image
-              //   src={comment.user.avatar || "noAvatar.png"}
-              src="/noAvatar.png"
-              alt=""
-              width={40}
-              height={40}
-              className="w-10 h-10 rounded-full"
-            />
-
-            {/* desc  */}
-            <div className="flex flex-col gap-2 flex-1">
-              <span className="font-medium">Satyakant Sahu</span>
-              <p>Nive pic!!</p>
-              <div className="flex items-center gap-8 text-xs text-gray-500 mt-2">
-                <div className="flex items-center gap-4">
-                  <Image
-                    src="/like.png"
-                    alt=""
-                    width={12}
-                    height={12}
-                    className="cursor-pointer w-4 h-4"
-                  />
-                  <span className="text-gray-300">|</span>
-                  <span className="text-gray-500">0 Likes</span>
-                </div>
-                <div className="">Reply</div>
-              </div>
-            </div>
-
-            {/* icon  */}
-            <Image
-              src="/more.png"
-              alt=""
-              width={16}
-              height={16}
-              className="cursor-pointer w-4 h-4"
-            />
-          </div>
-        </div>
-      </div>
+      {/* write  */}
+      <CommentList comments={comments} postId={postId} />
     </div>
   );
 };
