81040f2f7ce0c2e87f2f28ef33acd139d47a8080
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-28 12:48:48 +0530

    add userinfocard and usermediacard

--- src/components/rightMenu/RightMenu.tsx
+++ src/components/rightMenu/RightMenu.tsx
@@ -1,10 +1,24 @@
+import { Suspense } from "react";
 import Ad from "../Ad";
 import Birthdays from "./Birthdays";
 import FriendRequests from "./FriendRequests";
+import UserInfoCard from "./UserInfoCard";
+import UserMediaCard from "./UserMediaCard";
+import { User } from "@prisma/client";
 
-const RightMenu = ({ userId }: { userId?: string }) => {
+const RightMenu = ({ user }: { user?: User }) => {
   return (
     <div className="flex flex-col gap-6">
+      {user ? (
+        <>
+          <Suspense fallback="loading...">
+            <UserInfoCard user={user} />
+          </Suspense>
+          <Suspense fallback="loading...">
+            <UserMediaCard user={user} />
+          </Suspense>
+        </>
+      ) : null}
       <FriendRequests />
       <Birthdays />
       <Ad size="md" />
