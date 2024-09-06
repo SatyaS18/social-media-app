0c18c0eac39469a2ea2c06992bc0f49d4f55bfeb
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-06 22:53:05 +0530

    add stories function

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,10 +1,40 @@
+import { auth } from "@clerk/nextjs/server";
 import StoryList from "./StoryList";
+import prisma from "@/lib/client";
 
-const Stories = () => {
+const Stories = async () => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) return null;
+
+  const stories = await prisma.story.findMany({
+    where: {
+      expiresAt: {
+        gt: new Date(),
+      },
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
+          },
+        },
+        {
+          userId: currentUserId,
+        },
+      ],
+    },
+    include: {
+      user: true,
+    },
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
       <div className="flex gap-8 w-max">
-        <StoryList />
+        <StoryList stories={stories} userId={currentUserId} />
       </div>
     </div>
   );
